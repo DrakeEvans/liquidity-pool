8719d54caa27dafd155da0676711264ecdc4eb51
ADE <ade@ADEs-MBP.cable.rcn.com>
2021-11-02 15:53:44 -0400

    test(e2e liquidity pool testing): updated tests after swithcing implementation to use weth

--- test/e2e/add-liquidity.ts
+++ test/e2e/add-liquidity.ts
@@ -6,7 +6,7 @@ const {
   utils: { parseEther, parseUnits, formatEther },
 } = ethers;
 // const { parseEther, parseUnits } = utils;
-import { SpaceCoin, SpaceCoinIco, SpaceCoinEthPair, SpaceCoinRouter } from "../../typechain";
+import { SpaceCoin, SpaceCoinIco, SpaceCoinWethPair, SpaceCoinRouter, WrappedEth } from "../../typechain";
 
 async function deploy(contractName: string, args: any[] = []): Promise<any> {
   try {
@@ -23,14 +23,18 @@ async function deploy(contractName: string, args: any[] = []): Promise<any> {
 async function setupContracts() {
   const [owner, ...signers] = await ethers.getSigners();
   const spaceCoin: SpaceCoin = await deploy("SpaceCoin", [parseEther("500000")]);
+  const wrappedEther: WrappedEth = await deploy("WrappedEth");
   const spaceCoinIco: SpaceCoinIco = await deploy("SpaceCoinIco", [spaceCoin.address]);
-  const spaceCoinEthPair: SpaceCoinEthPair = await deploy("SpaceCoinEthPair", [spaceCoin.address]);
+  const SpaceCoinWethPair: SpaceCoinWethPair = await deploy("SpaceCoinWethPair", [
+    spaceCoin.address,
+    wrappedEther.address,
+  ]);
   const spaceCoinRouter: SpaceCoinRouter = await deploy("SpaceCoinRouter", [
     spaceCoin.address,
-    spaceCoinEthPair.address,
+    SpaceCoinWethPair.address,
+    wrappedEther.address,
   ]);
-  expect(spaceCoinEthPair.address).to.equal(await spaceCoinRouter.spaceCoinEthPairAddress());
-  await spaceCoinEthPair.setRouterAddresses(spaceCoinRouter.address, true).then(tx => tx.wait());
+  expect(SpaceCoinWethPair.address).to.equal(await spaceCoinRouter.spaceCoinWethPairAddress());
   await spaceCoin.transfer(spaceCoinIco.address, parseEther("500000")).then(tx => tx.wait());
   const balance = await spaceCoin.balanceOf(spaceCoinIco.address);
   expect(balance).to.equal(parseEther("500000"));
@@ -38,23 +42,31 @@ async function setupContracts() {
   await spaceCoinIco.movePhaseForward().then(tx => tx.wait());
   const promises = signers.slice(0, 3).map(async (address, index) => {
     try {
+      const wrappedEthPromise = wrappedEther
+        .connect(address)
+        .faucet(parseEther("5000"))
+        .then(tx => tx.wait());
       await spaceCoinIco
         .connect(address)
         .purchaseSpaceCoin({ value: parseEther("1000") })
         .then(tx => tx.wait());
       const balance = await spaceCoin.balanceOf(address.address);
       expect(balance).to.equal(parseEther("5000").toString());
+      await wrappedEthPromise;
+      const wethBalance = await wrappedEther.balanceOf(address.address);
+      expect(wethBalance).to.equal(parseEther("5000").toString());
     } catch (err) {
       throw err;
     }
   });
   await Promise.all(promises);
-  return { spaceCoin, spaceCoinIco, spaceCoinEthPair, spaceCoinRouter, owner, signers };
+  return { spaceCoin, wrappedEther, spaceCoinIco, SpaceCoinWethPair, spaceCoinRouter, owner, signers };
 }
 
 describe("SpaceCoin Router", () => {
   it("Should allow user to add liquidity", async () => {
-    const { spaceCoin, spaceCoinIco, spaceCoinEthPair, spaceCoinRouter, owner, signers } = await setupContracts();
+    const { spaceCoin, wrappedEther, spaceCoinIco, SpaceCoinWethPair, spaceCoinRouter, owner, signers } =
+      await setupContracts();
     const amountSpaceCoin = parseEther("1000");
     const amountEth = parseEther("200");
 
@@ -73,23 +85,39 @@ describe("SpaceCoin Router", () => {
       .increaseAllowance(spaceCoinRouter.address, amountSpaceCoin.mul(2))
       .then(tx => tx.wait());
 
-    const output = await spaceCoinRouter
+    // Give router access to weth
+    await wrappedEther
       .connect(signers[0])
-      .addLiquidity(amountSpaceCoin, "0", "0", signers[0].address, 50000000000, { value: amountEth })
+      .increaseAllowance(spaceCoinRouter.address, amountSpaceCoin)
+      .then(tx => tx.wait());
+
+    await wrappedEther
+      .connect(signers[1])
+      .increaseAllowance(spaceCoinRouter.address, amountSpaceCoin)
+      .then(tx => tx.wait());
+
+    await wrappedEther
+      .connect(signers[2])
+      .increaseAllowance(spaceCoinRouter.address, amountSpaceCoin.mul(2))
+      .then(tx => tx.wait());
+
+    await spaceCoinRouter
+      .connect(signers[0])
+      .addLiquidity(amountSpaceCoin, amountEth, "0", "0", signers[0].address, 50000000000)
       .then(tx => tx.wait());
     await spaceCoinRouter
       .connect(signers[1])
-      .addLiquidity(amountSpaceCoin, "0", "0", signers[1].address, 50000000000, { value: amountEth })
+      .addLiquidity(amountSpaceCoin, amountEth, "0", "0", signers[1].address, 50000000000)
       .then(tx => tx.wait());
     await spaceCoinRouter
       .connect(signers[2])
-      .addLiquidity(amountSpaceCoin.mul(2), "0", "0", signers[2].address, 50000000000, { value: amountEth.mul(2) })
+      .addLiquidity(amountSpaceCoin.mul(2), amountEth.mul(2), "0", "0", signers[2].address, 50000000000)
       .then(tx => tx.wait());
 
-    expect(await spaceCoinEthPair.ethReserves()).to.equal(parseEther("800"));
-    expect(await spaceCoinEthPair.spaceCoinReserves()).to.equal(parseEther("4000"));
-    const balance0 = await spaceCoinEthPair.balanceOf(signers[0].address);
-    const assertion = await spaceCoinEthPair.sqrt(parseEther("1000").mul(parseEther("200")));
+    expect(await SpaceCoinWethPair.wethReserves()).to.equal(parseEther("800"));
+    expect(await SpaceCoinWethPair.spaceCoinReserves()).to.equal(parseEther("4000"));
+    const balance0 = await SpaceCoinWethPair.balanceOf(signers[0].address);
+    const assertion = await SpaceCoinWethPair.sqrt(parseEther("1000").mul(parseEther("200")));
     expect(balance0).to.equal(assertion);
 
     const swap = async (signer: SignerWithAddress) => {
@@ -99,7 +127,7 @@ describe("SpaceCoin Router", () => {
 
       const amountSwappedSpaceCoin0 = await spaceCoinRouter
         .connect(signer)
-        .swapEthForSpaceCoin("0", signer.address, { value: parseEther("100") })
+        .swapWethForSpaceCoin("0", parseEther("100"), signer.address)
         .then(tx => tx.wait());
       const newAmountSpaceCoin = await spaceCoin.balanceOf(signer.address);
 
@@ -113,18 +141,17 @@ describe("SpaceCoin Router", () => {
     await swap(signers[1]);
     await swap(signers[2]);
 
-    const oldBalance = await spaceCoinEthPair.balanceOf(signers[0].address);
+    const oldBalance = await SpaceCoinWethPair.balanceOf(signers[0].address);
 
     const amountLP = parseEther("400");
-    await spaceCoinEthPair
-      .connect(signers[0])
+    await SpaceCoinWethPair.connect(signers[0])
       .increaseAllowance(spaceCoinRouter.address, amountLP)
       .then(tx => tx.wait());
     await spaceCoinRouter
       .connect(signers[0])
       .removeLiquidity(amountLP, "0", "0", signers[0].address, 500000000000000)
       .then(tx => tx.wait());
-    const newBalance = await spaceCoinEthPair.balanceOf(signers[0].address);
+    const newBalance = await SpaceCoinWethPair.balanceOf(signers[0].address);
     expect(oldBalance).to.be.gt(newBalance);
   });
 });
